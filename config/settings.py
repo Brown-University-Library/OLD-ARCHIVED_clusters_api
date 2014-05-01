5b61f5d85977e7b7486fe81fa9a04fdaa340526e
birkin <birkin.diana@gmail.com>
2014-05-01 15:17:27 -0400

    adds documentation url.

--- config/settings.py
+++ config/settings.py
@@ -3,12 +3,13 @@
 import json, os
 
 
-# auth
-LEGIT_IPS = json.loads( unicode(os.environ.get(u'clusters__LEGIT_IPS')) )
-
 # file-logger
 LOG_DIR = unicode( os.environ.get(u'clusters__LOG_DIR') )
 LOG_LEVEL = unicode( os.environ.get(u'clusters__LOG_LEVEL') )
+
+# web
+DOCUMENTATION_URL = unicode(os.environ.get(u'clusters__DOCUMENTATION_URL'))
+LEGIT_IPS = json.loads( unicode(os.environ.get(u'clusters__LEGIT_IPS')) )
 
 # grabber
 SOURCE_URL = unicode( os.environ.get(u'clusters__SOURCE_URL') )
